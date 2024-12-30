8e156e3706b96d0a416fcbbc24bf7e5c0bcf2a34
Ismaeil <ismaeilsaberi@gmail.com>
2024-12-30 01:55:35 -0800

    02-creating the layout of the project and main page

---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'AmazonShop'
+export const APP_SLOGAN =
+  process.env.NEXT_PUBLIC_APP_SLOGAN || 'For Best products, spend less.'
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  'An Amazon clone built with Next.js and MongoDB'
