46e4d75080fc30e1d8542a6bd8d8085ec9b98600
Ismaeil <ismaeilsaberi@gmail.com>
2025-01-03 06:30:27 -0800

    07-creating-product-details-page

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,9 @@
-import Header from '@/components/shared/header'
-import Footer from '@/components/shared/footer'
 import React from 'react'
 
-export default function HomeLayout({
+import Header from '@/components/shared/header'
+import Footer from '@/components/shared/footer'
+
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
@@ -10,7 +11,7 @@ export default function HomeLayout({
   return (
     <div className='flex flex-col min-h-screen'>
       <Header />
-      <main className='flex-1 flex flex-col'>{children}</main>
+      <main className='flex-1 flex flex-col p-4'>{children}</main>
       <Footer />
     </div>
   )
