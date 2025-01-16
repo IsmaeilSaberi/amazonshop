4c4aec400112109bee536bab67d2fff3cc15051a
Ismaeil <ismaeilsaberi@gmail.com>
2025-01-16 09:18:51 -0800

    20-create-order-history-page

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,31 @@
 import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
+import qs from 'query-string'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function formUrlQuery({
+  params,
+  key,
+  value,
+}: {
+  params: string
+  key: string
+  value: string | null
+}) {
+  const currentUrl = qs.parse(params)
+
+  currentUrl[key] = value
+
+  return qs.stringifyUrl(
+    {
+      url: window.location.pathname,
+      query: currentUrl,
+    },
+    { skipNull: true }
+  )
 }
 
 export const formatNumberWithDecimal = (num: number): string => {
