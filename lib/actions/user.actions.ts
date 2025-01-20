131b9cbefc20359d034b0f23f38cbe0b2060ea71
Ismaeil <ismaeilsaberi@gmail.com>
2025-01-20 09:55:43 -0800

    30-admin-users

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,10 +4,51 @@ import { auth, signIn, signOut } from '@/auth'
 import { IUserName, IUserSignIn, IUserSignUp } from '@/types'
 import { redirect } from 'next/navigation'
 import { connectToDataBase } from '../db'
-import User from '../db/models/user.model'
 import { UserSignUpSchema } from '../validator'
 import bcrypt from 'bcryptjs'
 import { formatError } from '../utils'
+import User, { IUser } from '../db/models/user.model'
+import { revalidatePath } from 'next/cache'
+import { PAGE_SIZE } from '../constants'
+
+// DELETE
+export async function deleteUser(id: string) {
+  try {
+    await connectToDataBase()
+    const res = await User.findByIdAndDelete(id)
+    if (!res) throw new Error('Use not found')
+    revalidatePath('/admin/users')
+    return {
+      success: true,
+      message: 'User deleted successfully',
+    }
+  } catch (error) {
+    return { success: false, message: formatError(error) }
+  }
+}
+
+// GET
+export async function getAllUsers({
+  limit,
+  page,
+}: {
+  limit?: number
+  page: number
+}) {
+  limit = limit || PAGE_SIZE
+  await connectToDataBase()
+
+  const skipAmount = (Number(page) - 1) * limit
+  const users = await User.find()
+    .sort({ createdAt: 'desc' })
+    .skip(skipAmount)
+    .limit(limit)
+  const usersCount = await User.countDocuments()
+  return {
+    data: JSON.parse(JSON.stringify(users)) as IUser[],
+    totalPages: Math.ceil(usersCount / limit),
+  }
+}
 
 // CREATE
 export async function registerUser(userSignUp: IUserSignUp) {
